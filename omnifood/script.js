b209c03552b7ef3dc73deaa5b33590ac8ec74642
Indrani <82924930+indra-rani@users.noreply.github.com>
2022-02-07 14:41:20 +0000

    wrote js for smooth scrolling & made mobile navigation work

--- omnifood/script.js
+++ omnifood/script.js
@@ -4,3 +4,38 @@ const myName = "Puja Kumari";
 const yearEl = document.querySelecter(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+/////// MAKE MOBILE NAVIGATION WORK ///////
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+/////// SMOOTH SCROLLING ANIMATION ////////
+
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
