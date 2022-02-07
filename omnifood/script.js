cfeed5285399d717625d4d8f9ceb1f5b0eaff250
Indrani <82924930+indra-rani@users.noreply.github.com>
2022-02-07 00:15:22 +0000

    created javaScriptFile and wrote some javaScript

---
+++ omnifood/script.js
@@ -0,0 +1,6 @@
+console.log("Hello world");
+const myName = "Puja Kumari";
+
+const yearEl = document.querySelecter(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
