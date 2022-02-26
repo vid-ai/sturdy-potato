14ae590b3fc164394f6ef5aa8d638ccda34b7f5f
Indrani <82924930+indra-rani@users.noreply.github.com>
2022-02-26 15:30:12 +0000

    wrote some js

---
+++ javascript_practice/script.js
@@ -0,0 +1,56 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+console.log(firstName);
+
+//
+/*
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//console.log (typeof type)
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year:1991;
+console.log(typeof year);
+console.log(typeof null);
+*/
+
+/*
+const ageJonas = 2037-1991;
+const ageSarah = 2037-2018;
+console.log(ageJonas, ageSarah);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + "" + lastName);
+
+//Assignment Operators
+
+let x = 10 + 5; //15
+x += 10; //x = x+10 = 25
+x *= 4; // x = x*4 = 100
+x++; // x = x+1 = 100
+x--;
+x--;
+console.log(x);
+
+//Comparision Operators
+
+console.log(ageJonas > ageSarah); //>,<,>=,<=
+console.log(ageSarah >= 18);
+
+console.log(now - 1991 > now - 2018);
