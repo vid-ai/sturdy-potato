bc5009cd09b49249419ef93d12747ff20f722a56
Indrani <82924930+indra-rani@users.noreply.github.com>
2022-03-07 17:22:58 +0000

    wrote some more code for amazon ui

--- javascript_practice/script.js
+++ javascript_practice/script.js
@@ -1,4 +1,5 @@
-let js = "amazing";
+/*
+ let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
 console.log("Jonas");
@@ -8,7 +9,7 @@ let firstName = "Matilda";
 console.log(firstName);
 
 //
-/*
+
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
 
@@ -22,13 +23,13 @@ console.log(typeof year);
 year:1991;
 console.log(typeof year);
 console.log(typeof null);
-*/
 
-/*
+
+
 const ageJonas = 2037-1991;
 const ageSarah = 2037-2018;
 console.log(ageJonas, ageSarah);
-*/
+
 
 const now = 2037;
 const ageJonas = now - 1991;
@@ -53,4 +54,18 @@ console.log(x);
 console.log(ageJonas > ageSarah); //>,<,>=,<=
 console.log(ageSarah >= 18);
 
+console.log(now - 1991 > now - 2018)
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
 console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; //x=y=10, x=10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+*/
